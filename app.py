8f285ae38a325fba017d025c8b651200035714d3
konstantindolbilov <dolbilovkonstantin@gmail.com>
2022-11-12 01:04:33 +1000

    Commit homework

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+
+import logging
 from flask import Flask, request, render_template, send_from_directory
 # from functions import ...
+from loader.views import loader_blueprint
+from main.views import main_blueprint
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -7,9 +11,10 @@ UPLOAD_FOLDER = "uploads/images"
 app = Flask(__name__)
 
 
-@app.route("/")
-def page_index():
-    pass
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+
+logging.basicConfig(filename='basic.log', level=logging.INFO)
 
 
 @app.route("/list")
